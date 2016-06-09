e4498d6e85390d9aeeff0d0ac7776f8435ceecfe
Adrian Fish <adrian.r.fish@gmail.com>
2016-06-09 17:25:30 +0100

    PROFILEPOPUP

--- reference/library/src/morpheus-master/js/src/sakai.morpheus.profile.popup.js
+++ reference/library/src/morpheus-master/js/src/sakai.morpheus.profile.popup.js
@@ -1,33 +1,36 @@
 var profilePopupObserver = new MutationObserver(function (records, observer) {
-                    console.log('HERE1');
 
         records.forEach(function (record) {
-                        console.log('HERE2');
             
             if (record.type === 'childList') {
-                        console.log('HERE3');
                 for (var i=0,j=record.addedNodes.length;i<j;i++) {
-                    console.log(record.addedNodes.item(i).class);
-                    var node = $PBJQ(record.addedNodes.item(i));
-                    if (node.hasClass('profile-popup')) {
-                        console.log('profile popup element added');
-                        node.qtip({
-                            position: { viewport: $PBJQ(window), adjust: { method: 'flipinvert none'} },
-                            show: { event: 'click', delay: 0 },
-                            style: { classes: 'commons-qtip qtip-rounded' },
-                            hide: { event: 'click unfocus' },
-                            content: {
-                                text: function (event, api) {
+                    var nakedNode = record.addedNodes.item(i);
 
-                                    return $PBJQ.ajax( { url: "/direct/profile/" + userId + "/formatted" })
-                                        .then(function (html) {
-                                                return html;
-                                            }, function (xhr, status, error) {
-                                                api.set('content.text', status + ': ' + error);
-                                            });
+                    console.log('type: ' + nakedNode.nodeType);
+                    if (nakedNode.nodeType === 1) {
+                        console.log('name: ' + nakedNode.nodeName);
+                        var node = $PBJQ(nakedNode);
+                        if (node.hasClass('profile-popup')) {
+                            console.log("NAME : " + node.nodeName);
+                            console.log('profile popup element added');
+                            node.qtip({
+                                position: { viewport: $PBJQ(window), adjust: { method: 'flipinvert none'} },
+                                show: { event: 'click', delay: 0 },
+                                style: { classes: 'commons-qtip qtip-rounded' },
+                                hide: { event: 'click unfocus' },
+                                content: {
+                                    text: function (event, api) {
+
+                                        return $PBJQ.ajax( { url: "/direct/profile/" + userId + "/formatted" })
+                                            .then(function (html) {
+                                                    return html;
+                                                }, function (xhr, status, error) {
+                                                    api.set('content.text', status + ': ' + error);
+                                                });
+                                    }
                                 }
-                            }
-                        });
+                            });
+                        }
                     }
                 }
             }
