46d6bd63962edb52b7d3dcd516b4158891b5fd7b
Adrian Fish <adrian.r.fish@gmail.com>
2021-09-02 19:37:01 +0100

    SAK-45092 Create a new tool for handling conversations in Sakai (#9500)
    https://jira.sakaiproject.org/browse/SAK-45092
    (cherry picked from commit 4b3c9eba2fce1c0365c6e611e3f71a00d419fa47)

--- webcomponents/tool/src/main/frontend/js/sakai-permissions.js
+++ webcomponents/tool/src/main/frontend/js/sakai-permissions.js
@@ -69,6 +69,7 @@ class SakaiPermissions extends SakaiElement {
       roles: { attribute: false, type: Array },
       groups: { attribute: false, type: Array },
       error: { attribute: false, type: String },
+      fireEvent: { attribute: "fire-event", type: Boolean },
     };
   }
 
@@ -93,7 +94,7 @@ class SakaiPermissions extends SakaiElement {
     if (this.roles) {
       return html`
 
-        ${this.groups ? html`
+        ${this.groups && this.groups.length > 0 ? html`
           <div>
             <label for="permissons-group-picker">${this.i18n["per.lis.selectgrp"]}</label>
             <sakai-group-picker id="permissions-group-picker" groups="${JSON.stringify(this.groups)}" @group-selected=${this.groupSelected} />
@@ -131,7 +132,7 @@ class SakaiPermissions extends SakaiElement {
         </table>
         <div class="act">
           <input type="button" class="active" value="${this.i18n["gen.sav"]}" aria-label="${this.i18n["gen.sav"]}" @click=${this.savePermissions}/>
-          <input type="button" value="${this.i18n["gen.can"]}" aria-label="${this.i18n["gen.can"]}" @click=${this.cancelPermissions}/>
+          <input type="button" value="${this.i18n["gen.can"]}" aria-label="${this.i18n["gen.can"]}" @click=${this._completePermissions}/>
           <span id="${this.tool}-failure-message" class="permissions-save-message" style="display: none;">${this.i18n["per.error.save"]}</span>
         </div>
       `;
@@ -171,7 +172,7 @@ class SakaiPermissions extends SakaiElement {
 
     document.body.style.cursor = "wait";
 
-    const boxes = document.querySelectorAll(`#${this.tool.replace('.', '\\.')}-permissions-table input[type="checkbox"]`);
+    const boxes = this.querySelectorAll(`#${this.tool.replace('.', '\\.')}-permissions-table input[type="checkbox"]`);
     const params = `ref=${this.groupReference}&` + Array.from(boxes).reduce((acc,b) => {
 
       if (b.checked) {
@@ -185,11 +186,7 @@ class SakaiPermissions extends SakaiElement {
       .then(res => {
 
         if (res.ok) {
-          if (this.onRefresh) {
-            window.location.href = this.onRefresh;
-          } else {
-            window.location.reload();
-          }
+          this._completePermissions();
         } else {
           throw new Error("Network response was not ok.");
         }
@@ -222,8 +219,11 @@ class SakaiPermissions extends SakaiElement {
     });
   }
 
-  cancelPermissions() {
-    if (this.onRefresh) {
+  _completePermissions() {
+
+    if (this.fireEvent) {
+      this.dispatchEvent(new CustomEvent("permissions-complete"));
+    } else if (this.onRefresh) {
       window.location.href = this.onRefresh;
     } else {
       window.location.reload();
